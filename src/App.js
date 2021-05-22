c0489b9185c6e546238f5f08acb76c09adf253bb
ericcapiz <ericcapiz@gmail.com>
2021-05-22 16:17:27 -0500

    chat set up with 2 users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height="100vh" 
+      projectID="b51416c6-17f7-42db-b5b8-6e4734746ccc" 
+      userName="hatzhero85" 
+      userSecret="test123"
+      renderChatfeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} 
+    />
   );
 }
 
